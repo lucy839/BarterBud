ca3daad9a56a4215b700595bd8dd8bbb056f1adf
Youngju Lee <lucylee839@hotmail.com>
2019-09-13 11:39:04 -0700

    background image added

---
+++ client/public/background.js
@@ -0,0 +1,41 @@
+var video = document.querySelector('video')
+, container = document.querySelector('#backgroundContainer');
+
+var setVideoDimensions = function () {
+// Video's intrinsic dimensions
+var w = video.videoWidth
+  , h = video.videoHeight;
+
+// Intrinsic Ratio
+// Will be more than 1 if W > H and less if W < H
+var videoRatio = (w / h).toFixed(2);
+
+// Get the container's computed styles
+//
+// Also calculate the min dimensions required (this will be
+// the container dimentions)
+var containerStyles = window.getComputedStyle(container)
+  , minW = parseInt( containerStyles.getPropertyValue('width') )
+  , minH = parseInt( containerStyles.getPropertyValue('height') );
+var widthRatio = minW / w
+  , heightRatio = minH / h;
+
+// Whichever ratio is more, the scaling
+// has to be done over that dimension
+if (widthRatio > heightRatio) {
+  var newWidth = minW;
+  var newHeight = Math.ceil( newWidth / videoRatio );
+}
+else {
+  var newHeight = minH;
+  var newWidth = Math.ceil( newHeight * videoRatio );
+}
+
+video.style.width = newWidth + 'px';
+video.style.height = newHeight + 'px';
+};
+
+video.addEventListener('loadedmetadata', setVideoDimensions, false);
+window.addEventListener('resize', setVideoDimensions, false);
+
+Resources
