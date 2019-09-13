2568499e7963432ce7f520e384fe5054f29f4843
Youngju Lee <lucylee839@hotmail.com>
2019-09-13 09:41:54 -0700

    created sign in and sign up button

--- client/src/pages/LoginPage.js
+++ client/src/pages/LoginPage.js
@@ -1,10 +1,14 @@
-import React from 'react';
-
-function LoginPage (){
-    return(
-        <div>
-            test2
-        </div>
-    );
+import React, { Component } from 'react';
+// link signup page and sign in page
+class LoginPage extends Component {
+    render(){
+        return(
+            // signup button and sign in button
+            <div>
+                <button>Login</button>
+                <button>Sign Up</button>
+            </div>
+        );
+    }
 }
 export default LoginPage;
