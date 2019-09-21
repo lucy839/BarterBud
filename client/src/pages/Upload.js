dcab676c866fdd9fef89eb0de0ddcd16ca6850db
Youngju Lee <lucylee839@hotmail.com>
2019-09-21 10:01:34 -0700

    made the function as stateful

--- client/src/pages/Upload.js
+++ client/src/pages/Upload.js
@@ -1,47 +1,50 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-function Upload() {
-    return (
-        <div>
-            <h2 id="upload">Upload</h2>
-            <form class="create-form" >
-                <div class="form-group">
-                    <label for="ca">Product name:</label>
-                    <input type="text" id="product_name" name="product_name" required />
-                    <div class="invalid-feedback">
-                        Please enter product name.
+class Upload extends Component {
+    // constructor() {
+    state = {
+        name: '',
+        condition: '',
+        description:''
+    }     
+    // }
+    render() {
+        return (
+            <div>
+                <h2 id="upload">Upload</h2>
+                <form class="create-form" >
+                    <div class="form-group">
+                        <label for="ca">Product name:</label>
+                        <input type="text" id="product_name" name="product_name" required />
+                        <div class="invalid-feedback">
+                            Please enter product name.
+                            </div>
+                    </div>
+                    <div class="form-group">
+                        <label for="condition">Condition:</label>
+                        <select class="form-control" id="condition" required>
+                            <option>New</option>
+                            <option>Great</option>
+                            <option>Fair</option>
+                            <option>Okay</option>
+                            <option>Bad</option>
+                        </select>
+                        <div class="invalid-feedback">
+                            Please choose the condition of product.
                         </div>
-                </div>
-                <div class="form-group">
-                    <label for="condition">Condition:</label>
-                    <select class="form-control" id="condition" required>
-                        <option>New</option>
-                        <option>Great</option>
-                        <option>Fair</option>
-                        <option>Okay</option>
-                        <option>Bad</option>
-                    </select>
-                    <div class="invalid-feedback">
-                        Please choose the condition of product.
                     </div>
-                </div>
-                <div class="form-group">
-                    <label for="ca">Description:</label>
-                    <input type="text" id="description" name="description" required />
-                    <div class="invalid-feedback">
-                        Please enter description of product.
+                    <div class="form-group">
+                        <label for="ca">Description:</label>
+                        <input type="text" id="description" name="description" required />
+                        <div class="invalid-feedback">
+                            Please enter description of product.
+                        </div>
                     </div>
-                </div>
-                <div class="form-group">
-                    <label for="contact">Email address</label>
-                    <input type="email" class="form-control" id="contact" placeholder="name@example.com" required />
-                    <div class="invalid-feedback">
-                        Please enter email address for contact
-                    </div>
-                </div>
-            </form>
-        </div>
-
-    );
+                    <button id="submit" type="submit">submit</button>
+                </form>
+            </div>
+    
+        );
+    }
 }
 export default Upload;
