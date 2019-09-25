1741bcc101ef9a0268914acebf55c63fba4e225a
Youngju Lee <lucylee839@hotmail.com>
2019-09-25 15:21:24 -0700

    about page code cleaned

--- client/src/pages/About.js
+++ client/src/pages/About.js
@@ -1,14 +1,11 @@
 import React from "react";
-// import Nav from "../components/Nav";
 import Container from "../components/Container";
 
+// simple about page
 function About() {
     return (
-<Container>
-        <div>
-            {/* redundacy that needs to be fixed */}
-            {/* <Nav></Nav> */}
-            {/* <Container> */}
+        <Container>
+            <div>
                 <h1 className="text-center">
                     What is <strong>Barter Bud</strong>?
                 </h1>
@@ -18,19 +15,19 @@ function About() {
                     age, we have lost touch with this basic root of human civilization.
                     <br></br>
                     Introducing Barter Bud!
-                    <br></br> 
+                    <br></br>
                     Barter Bud allows users to create a
                     profile where they can upload pictures of things or services
                     they would like to trade and can interact and make offers with
                     other users for trading said items or services.
                     <br></br>
-                    Barter bud helps the community. 
+                    Barter bud helps the community.
                     <br></br>
                     Barter Bud helps all the community with decluttering homes, offices, yards to make a better aesthetic backgrounds to have a peaceful mind. It is so important for our environment to Reduce landfills, with Reuse and Recycle Items as much as possible. Recycling saves energy, it also reduces greenhouse gas emissions. The community feels stronger connections since they interact and helping each other.
                   </p>
-            {/* </Container> */}
-        </div>
-        </Container>    
+            </div>
+        </Container>
     );
 }
+
 export default About;
