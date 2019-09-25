fc7a3d64eef07157e7bb9042a65aca13d8191b18
Youngju Lee <lucylee839@hotmail.com>
2019-09-25 15:25:24 -0700

    signup and signin moved to components

--- client/src/pages/LoginPage.js
+++ client/src/pages/LoginPage.js
@@ -2,8 +2,8 @@ import React, { Component } from "react";
 import Modal, { ModalHeader, ModalBody } from "../components/Modal/index";
 import Jumbotron from "../components/Jumbotron/index"
 // link signup page and sign in page
-import Signin from "./Signin";
-import Signup from "./Signup";
+import Signin from "../components/Signin/index";
+import Signup from "../components/Signup/index";
 import "./style.css";
 
 class LoginPage extends Component {
@@ -40,45 +40,9 @@ class LoginPage extends Component {
             default:
         }
         var modal = "";
-        switch (this.state.modal){
+        switch (this.state.modal) {
             case true:
-                modal =   <Modal isOpen={this.state.modal} className="modal">
-                <ModalHeader>
-                    <button
-                        type="button"
-                        className="close"
-                        aria-label="Close"
-                        onClick={this.toggle}
-                    >
-                        <span aria-hidden="true">&times;</span>
-                    </button>
-                </ModalHeader>
-                <br></br>
-                <ModalBody>
-                    {form}
-                </ModalBody>
-            </Modal>
-                 break
-       
-            case false :
-                modal =    <Jumbotron>
-                <div>
-                    <h1 class="intro">Welcome to Barter Bud <i class="fas fa-sync-alt"></i><div id="#user"></div></h1>
-                    <br></br>
-                    <button name="button" type="button" className="btn" value="signup" onClick={this.toggle}>Sign Up</button>
-                    {/* If login button is clicked, have login page pop up */}
-                    <button name="button" type="button" className="btn" value="signin" onClick={this.toggle}>Login</button>
-                </div>
-            </Jumbotron>
-                 break
-             default:
-        }
-
-        return (
-            // signup button and sign in button
-            <div>
-                {/* If sign up button is clicked, have sign up page pop up */}
-                {/* <Modal isOpen={this.state.modal} className="modal">
+                modal = <Modal isOpen={this.state.modal} className="modal">
                     <ModalHeader>
                         <button
                             type="button"
@@ -89,23 +53,33 @@ class LoginPage extends Component {
                             <span aria-hidden="true">&times;</span>
                         </button>
                     </ModalHeader>
+                    <br></br>
                     <ModalBody>
                         {form}
                     </ModalBody>
-                </Modal> */}
-                {/* <Jumbotron>
+                </Modal>
+                break
+
+            case false:
+                modal = <Jumbotron>
                     <div>
                         <h1 class="intro">Welcome to Barter Bud <i class="fas fa-sync-alt"></i><div id="#user"></div></h1>
                         <br></br>
-                        <button name="button" type="button" className="btn" value="signup" onClick={this.toggle}>Sign Up</button> */}
+                        <button name="button" type="button" className="btn" value="signup" onClick={this.toggle}>Sign Up</button>
                         {/* If login button is clicked, have login page pop up */}
-                        {/* <button name="button" type="button" className="btn" value="signin" onClick={this.toggle}>Login</button>
+                        <button name="button" type="button" className="btn" value="signin" onClick={this.toggle}>Login</button>
                     </div>
-                </Jumbotron> */}
-    {modal}
-             
+                </Jumbotron>
+                break
+            default:
+        }
+
+        return (
+            <div>
+                {modal}
             </div>
         );
     }
 }
+
 export default LoginPage;
