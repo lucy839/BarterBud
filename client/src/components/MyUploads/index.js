7f6acd9b7b9a901ea243c5ee4d47e8f712a21a6b
Youngju Lee <lucylee839@hotmail.com>
2019-09-25 20:31:40 -0700

    myuploads code cleaned up

--- client/src/components/MyUploads/index.js
+++ client/src/components/MyUploads/index.js
@@ -6,43 +6,36 @@ class MyUploads extends Component {
     constructor(props) {
         super(props)
         this.state = {
-            redirectTo:null,
-            trade:""
+            redirectTo: null,
+            trade: ""
         }
         this.back = this.back.bind(this)
-        this.trade =this.trade.bind(this)
-
+        this.trade = this.trade.bind(this)
     }
-    // console.log(props.myProducts)
-    back(){
+
+    // when function is called, call closeRequest function to close the modal and redirect to market page
+    back() {
         this.props.closeRequest();
-        this.setState({redirectTo: '/market' })
-
+        this.setState({ redirectTo: '/market' })
     }
-    // when this button is clicked, update status of both the user's and the product's user status to process
-    trade(event){
-        console.log(this.props.request)
-        // ,event.target.value
+
+    // when this button is clicked, update status of both the user's and the product's user status to process 
+    //  in the database
+    trade(event) {
         let test = [this.props.request, event.target.value]
-        API.requestTrade(test).then(res =>
-            // conssole.log(res)
-            // this.setState({ products: res.data})
-           
-          alert("Request Sent!"),
-          this.back()
-        )
-        .catch(err => console.log(err))
+        API.requestTrade(test)
+            .then(
+                alert("Request Sent!"),
+                this.back()
+            )
+            .catch(err => console.log(err))
     }
-    // return (
-    render() {
-        console.log(this.props.user)
-        
-        let back = <button className="btn trade" type="submit" onClick = {this.back}>back</button>;
-        let list = this.props.myProducts.map(product => {
-            console.log(product)
-            // console.log(myProduct.productname)
-            return (<div>
 
+    render() {
+        // format my products from props
+        let back = <button className="btn trade" type="submit" onClick={this.back}>back</button>;
+        let list = this.props.myProducts.map(product => {
+            return (<div>
                 <h1> product name: </h1>
                 <p>{product.productname}</p>
                 <h1>condition</h1>
@@ -51,46 +44,31 @@ class MyUploads extends Component {
                 <p>{product.description}</p>
                 <button
                     className="btn trade"
-                    onClick = {this.trade}
-                    // name = "trade"
-                    value = {product._id}
+                    onClick={this.trade}
+                    value={product._id}
                     type="submit">trade</button>
             </div>
             );
         });
+
         if (this.state.redirectTo) {
             return <Redirect to={{ pathname: this.state.redirectTo }} />
         } else {
-        // if there is no books saved, display this
-        return (this.props.myProducts.length === 0) ? (
-            <div>
-                {back}
-                <h5>No Products to Trade</h5>
-
-            </div>
-        ) : (
-                // else display books saved
-                <div>
-                    {back}
-                    {list}
-                    {/* this.props.myProducts.map(product => {
-                return (<div>
-                    
-                    <h1> product name: </h1>
-                    <p>{product.productname}</p>
-                    <h1>condition</h1>
-                    <p>{product.condition}</p>
-                    <h1>description</h1>
-                    <p>{product.description}</p>
-                    <button
-                        className="btn trade"
-                        // onClick = {this.tradeRequest}
-                        type="submit">trade</button>
-                </div>
-                );
-            })*/}
-                </div>)
-
-    }}
+            // if there is no product, display no products to trade, else display list
+            return (this.props.myProducts.length === 0) ?
+                (
+                    <div>
+                        {back}
+                        <h5>No Products to Trade</h5>
+                    </div>
+                ) : (
+                    <div>
+                        {back}
+                        {list}
+                    </div>
+                )
+        }
+    }
 }
+
 export default MyUploads
