5b3013dd0c3652bd33e19b966772c3ca931e3436
Youngju Lee <lucylee839@hotmail.com>
2019-09-25 20:27:32 -0700

    products code cleaned

--- client/src/components/Products/index.js
+++ client/src/components/Products/index.js
@@ -1,68 +1,22 @@
-import React, {Component} from 'react';
-import { Redirect } from 'react-router-dom'
+import React, { Component } from 'react';
 
 class Products extends Component {
     constructor(props) {
         super(props)
-        // this.state = {
-        //     request : ""
-        // }
-        this.handleSubmit = this.handleSubmit.bind(this)
-
-    }
-    // console.log(props.myProducts)
-    handleSubmit(event){
-        event.preventDefault()
-		// this.setState({
-		// 	[event.target.name]: event.target.value
-        // })
-        console.log(event.target.value)
-        this.props.tradeRequest(		[event.target.name], event.target.value)
-
+        this.handleSubmit = this.handleSubmit.bind(this);
     }
 
-    // return (
+    // when button is clicked, call tradeRequest function from props
+    handleSubmit(event) {
+        event.preventDefault();
+        this.props.tradeRequest([event.target.name], event.target.value);
+    }
+
     render() {
-        // console.log(this.state.request) 
+        // format the products from props
         let list = this.props.products.map(product => {
-            console.log(product.user, this.props.user)
-            console.log(product._id)
-            // console.log(myProduct.productname)
-                if (!(product.user == this.props.user)){
-
-            return (<div>
-
-                <h1> product name: </h1>
-                <p>{product.productname}</p>
-                <h1>condition</h1>
-                <p>{product.condition}</p>
-                <h1>description</h1>
-                <p>{product.description}</p>
-                <button
-                    className="btn trade"
-                    name = "request"
-                    value = {product._id}
-                    onClick = {this.handleSubmit}
-                    type="submit">request</button>
-            </div>
-            );}
-        });
-     
-        // if there is no books saved, display this
-        return (this.props.products.length === 0) ? (
-            <div>
-               
-                <h5>No Products to Trade</h5>
-
-            </div>
-        ) : (
-                // else display books saved
-                <div>
-                
-                    {list}
-                    {/* this.props.myProducts.map(product => {
+            if (!(product.user == this.props.user)) {
                 return (<div>
-                    
                     <h1> product name: </h1>
                     <p>{product.productname}</p>
                     <h1>condition</h1>
@@ -71,13 +25,27 @@ class Products extends Component {
                     <p>{product.description}</p>
                     <button
                         className="btn trade"
-                        // onClick = {this.tradeRequest}
-                        type="submit">trade</button>
+                        name="request"
+                        value={product._id}
+                        onClick={this.handleSubmit}
+                        type="submit">request</button>
                 </div>
                 );
-            })*/}
-                </div>)
+            }
+        });
 
+        // if there is no product, display no products to trade, else display list
+        return (this.props.products.length === 0) ?
+            (
+                <div>
+                    <h5>No Products to Trade</h5>
+                </div>
+            ) : (
+                <div>
+                    {list}
+                </div>
+            )
     }
 }
+
 export default Products
