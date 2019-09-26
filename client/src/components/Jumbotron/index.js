26ac307f038569034470873558329c1877da698d
Youngju Lee <lucylee839@hotmail.com>
2019-09-25 19:42:40 -0700

    jumbotron code cleaned up

--- client/src/components/Jumbotron/index.js
+++ client/src/components/Jumbotron/index.js
@@ -1,16 +1,18 @@
 import React from 'react';
 
+// Display jumbotron
 function Jumbotron({ children }) {
-    return (
-      
-      <div
-        style={{clear: "both", textAlign: "center", overflow: "scroll", background:"white", 
-          height: "300px", width: "40%","margin-top":"10%", "margin-left":"30%"}}
-        className="jumbotron"
-      >
-        {children}
-      </div>
-    );
-  }
-  
-  export default Jumbotron;
+  return (
+    <div
+      style={{
+        clear: "both", textAlign: "center", overflow: "scroll", background: "white",
+        height: "300px", width: "40%", "margin-top": "10%", "margin-left": "30%"
+      }}
+      className="jumbotron"
+    >
+      {children}
+    </div>
+  );
+}
+
+export default Jumbotron;
