16986df56d6bcebace7b8f2faf7d1cedb7be0853
Youngju Lee <lucylee839@hotmail.com>
2019-09-25 21:24:17 -0700

    request code cleaned

--- client/src/components/Request/index.js
+++ client/src/components/Request/index.js
@@ -1,89 +1,65 @@
-import React, {Component} from 'react';
-import { Redirect } from 'react-router-dom';
-import API from '../../utils/API';
+import React, { Component } from "react";
+import API from "../../utils/API";
 
 class Request extends Component {
     constructor(props) {
         super(props)
         this.state = {
-            accept : ""
+            accept: ""
         }
         this.handleSubmit = this.handleSubmit.bind(this)
-
     }
-    // console.log(props.myProducts)
-    handleSubmit(event){
-        console.log(this.props.thisProduct)
+
+    // when button is clicked, set state with product info and call traded function
+    handleSubmit(event) {
         event.preventDefault()
-		this.setState({
-			[event.target.name]: event.target.value
-        })
-        console.log(event.target.value)
-        this.traded(event.target.value);
-        // this.props.tradeRequest([event.target.name], event.target.value)
-
-    }
-    traded (value){
-        let products = [value, this.props.thisProduct];
-        // let users ;
-        API.traded(products).then(res =>
-            // res => console.log(res)
-            API.switchUser([this.props.user,res.data.user]).then(
-                res=> console.log(res)
-            )
-            // alert("Please c  ommunicate via "+ this.props.email)  
-             ).catch(err => console.log(err))
-    }
-
-    // return (
-    render() {
-        // let button    ;
-        // let data = this.props.products.data 
-        // *****
-        console.log(this.props.user )
-        console.log(this.props.products) 
-        let list = this.props.products.map(product => {
-            // console.log(product.user, this.props.user)
-            console.log(product.requestFrom)
-            // if (product.status == "process"){
-            //     button = <button
-            //         className = "btn"
-            //         name = "requested"
-            //         value = {product.requestFrom}
-            //         onClick = {this.handleSubmit}
-            //         type = "submit">Trade Requested</button>
-            // }
-            // console.log(myProduct.productname)
-                // if (product.user == this.props.user){
-                
-                return (<div>
-
-                <h1> product name: </h1>
-                <p>{product.productname}</p>
-                <h1>condition</h1>
-                <p>{product.condition}</p>
-                <h1>description</h1>
-                <p>{product.description}</p>
-                <h1>email</h1>
-                <p>{this.props.email}</p>
-                {/* {button} */}
-                <button
-                    className="btn trade"
-                    name = "accept"
-                    value = {product._id}
-                    onClick = {this.handleSubmit}
-                    type="submit">Accept</button>
-            </div> 
-            );
-        // } 
+        this.setState({
+            [event.target.name]: event.target.value
         });
-     
-        // if there is no books saved, display this
-        return (<div>   {list}</div>
-                
-                 
-        )
+        this.traded(event.target.value);
+    }
 
+    // when called, trade product and user in the database
+    traded(value) {
+        let products = [value, this.props.thisProduct];
+        API.traded(products).then(res =>
+            API.switchUser([this.props.user, res.data.user])
+                .then(
+                    res => console.log(res)
+                )
+        ).catch(err => console.log(err))
+    }
+
+    render() {
+        // display the product
+        let list = this.props.products.map(product => {
+            return (
+                <div>
+                    <h1> product name: </h1>
+                    <p>{product.productname}</p>
+                    <h1>condition</h1>
+                    <p>{product.condition}</p>
+                    <h1>description</h1>
+                    <p>{product.description}</p>
+                    <h1>email</h1>
+                    <p>{this.props.email}</p>
+                    <button
+                        className="btn trade"
+                        name="accept"
+                        value={product._id}
+                        onClick={this.handleSubmit}
+                        type="submit">Accept
+                    </button>
+                </div>
+            );
+        });
+
+        return (
+            <div>
+                {list}
+            </div>
+        );
     }
 }
+
 export default Request
