7d1e8bfa38258291213b82d635eddb337b3102af
Youngju Lee <lucylee839@hotmail.com>
2019-09-25 19:44:24 -0700

    containerr code cleaned up

--- client/src/components/Container/index.js
+++ client/src/components/Container/index.js
@@ -1,16 +1,18 @@
 import React from 'react';
 
+// Hold all the contents of the app
 function Container({ children }) {
-    return (
-      
-      <div
-        style={{clear: "both", textAlign: "center", overflow: "scroll", background:"white", 
-          height: "550px", width: "80%","margin-top":"7%", "margin-left":"10%",
-        padding:"5%"}}
-     className = "container"  >
-        {children}
-      </div>
-    );
-  }
-  
-  export default Container;
+  return (
+    <div
+      style={{
+        clear: "both", textAlign: "center", overflow: "scroll", background: "white",
+        height: "550px", width: "80%", "margin-top": "7%", "margin-left": "10%",
+        padding: "5%"
+      }}
+      className="container"  >
+      {children}
+    </div>
+  );
+}
+
+export default Container;
